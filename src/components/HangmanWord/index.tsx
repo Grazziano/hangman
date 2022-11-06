1c850a39eb72f63518d66f0ec90752478844f93f
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-11-06 15:09:14 -0300

    Creating word component

--- src/components/HangmanWord/index.tsx
+++ src/components/HangmanWord/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import './HangmanWord.css';
 
 function HangmanWord() {
-  return <h1>HangmanWord</h1>;
+  const word = 'test';
+  const guessedLetters = ['e'];
+
+  return (
+    <div className="hangmanWordContainer">
+      {word.split('').map((letter, index) => {
+        return (
+          <span key={index} className="word">
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? 'visible'
+                  : 'hidden',
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 }
 
 export default HangmanWord;
