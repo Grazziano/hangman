5664e720670126416b49aa42a3f47e22b6f2e36b
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-11-06 15:40:27 -0300

    Creating component Keyboard

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import styles from './Keyboard.module.css';
+
+const KEYS = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
 
 function Keyboard() {
-  return <h1>Keyboard</h1>;
+  return (
+    <div className={styles.keyboardContainer}>
+      {KEYS.map((key) => {
+        return (
+          <button className={`${styles.btn}`} key={key}>
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Keyboard;
